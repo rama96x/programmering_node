bd5948563aac1bc55afaf0611406fb87b5b0ac12
karo19ab <karo19ab@student.cbs.dk>
2020-03-16 11:15:36 +0100

    Creation of class "Product"

---
+++ startbootstrap-clean-blog-gh-pages/models/Product.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+    productName: String,
+    price: Number
+});
+
+const Product = mongoose.model('Product',ProductSchema);
+module.exports = Product;
+
