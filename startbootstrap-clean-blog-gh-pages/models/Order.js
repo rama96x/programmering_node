9276d979e1b71808d22f069e5c28bbaefd548ec5
Jonathan <jomo19ad@student.cbs.dk>
2020-03-16 13:18:16 +0100

    commit order

---
+++ startbootstrap-clean-blog-gh-pages/models/Order.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+    orderID: mongoose.Schema.Types.ObjectId,
+    status: String, // måske boolean i stedet.
+    deliveryLocation: String, // kommentar til hvor man er.
+    lineItem: { type: mongoose.Schema.Types.ObjectId, ref: 'LineItem', required: true }, // Ret til, når LineItem er klar
+    billingAddress: {
+        customerID: { type: mongoose.Schema.Types.ObjectId, ref: 'customerID', required: true }, // Ret til, når User er klar
+        streetName: String,
+        streetNr: Number,
+        zipCode: Number,
+        city: String,
+        country: String,
+    },
+   lineStander:
+   
+
+
+});
+
+const Order = mongoose.model('Order',orderSchema);
+module.exports = Order;
+
