a3e7bb1a96d02dc1ef852416630a4ddccd288064
Jonathan <jomo19ad@student.cbs.dk>
2020-03-18 09:21:01 +0100

    commit order 3

--- startbootstrap-clean-blog-gh-pages/controllers/calculateOrderTotal.js
+++ startbootstrap-clean-blog-gh-pages/controllers/calculateOrderTotal.js
@@ -2,16 +2,15 @@
 
 // (Quantity * Price) + LS Fee
 
-const lsFee = 30;
 
 var quantity = { type: mongoose.Schema.Types.ObjectId, ref: 'quantity', required: true }; // skal rettes til når lineItem er klar
 var price =  { type: mongoose.Schema.Types.ObjectId, ref: 'price', required: true }; // skal rettes til når lineItem er klar
 
-const items = quantity * price;
+let items = quantity * price;
 
 
 function calculateOrderTotal(){
-    let orderTotal = 30;
+    let orderTotal = 30; // LS fee
         orderTotal += items;
     console.log(orderTotal);
 }
